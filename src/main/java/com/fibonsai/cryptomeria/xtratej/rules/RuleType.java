fc76749e51c46ca4293612f8f4a4d5180d1c7434
tuxmonteiro <me@tuxmonteiro.dev>
2026-02-27 09:28:41 +0100

    Add FalseRule

--- src/main/java/com/fibonsai/cryptomeria/xtratej/rules/RuleType.java
+++ src/main/java/com/fibonsai/cryptomeria/xtratej/rules/RuleType.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2026 fibonsai.com
+ *  All rights reserved.
+ *
+ *  This source is subject to the Apache License, Version 2.0.
+ *  Please see the LICENSE file for more information.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.fibonsai.cryptomeria.xtratej.rules;
 
 import com.fibonsai.cryptomeria.xtratej.event.ITemporalData;
@@ -13,6 +27,7 @@ public enum RuleType {
     And(AndRule.class),
     Crossed(CrossedRule.class),
     DateTime(DateTimeRule.class),
+    False(FalseRule.class),
     InSlope(InSlopeRule.class),
     Limit(LimitRule.class),
     Not(NotRule.class),
