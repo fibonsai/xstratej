bb449e12c94c006983a5d9ce571875543069b725
tuxmonteiro <me@tuxmonteiro.dev>
2026-02-27 21:04:25 +0100

    Fix LimitRuleTest

--- src/test/java/com/fibonsai/cryptomeria/xtratej/rules/impl/LimitRuleTest.java
+++ src/test/java/com/fibonsai/cryptomeria/xtratej/rules/impl/LimitRuleTest.java
@@ -1,8 +1,22 @@
 
+/*
+ *  Copyright (c) 2026 fibonsai.com
+ *  All rights reserved.
+ *
+ *  This source is subject to the Apache License, Version 2.0.
+ *  Please see the LICENSE file for more information.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.fibonsai.cryptomeria.xtratej.rules.impl;
 
-import com.fibonsai.cryptomeria.xtratej.event.reactive.Fifo;
 import com.fibonsai.cryptomeria.xtratej.event.ITemporalData;
+import com.fibonsai.cryptomeria.xtratej.event.reactive.Fifo;
 import com.fibonsai.cryptomeria.xtratej.event.series.impl.BooleanSingleTimeSeries.BooleanSingle;
 import com.fibonsai.cryptomeria.xtratej.event.series.impl.SingleTimeSeries;
 import com.fibonsai.cryptomeria.xtratej.event.series.impl.SingleTimeSeries.Single;
@@ -81,7 +95,7 @@ class LimitRuleTest {
     @Test
     void predicate_withinSourceBounds_shouldReturnTrue() {
         properties.put("lowerSourceId", "lower");
-        properties.put("topSourceId", "top");
+        properties.put("upperSourceId", "top");
         properties.set("sources", JsonNodeFactory.instance.arrayNode().add("s1"));
         LimitRule rule = new LimitRule("test", properties, mockResults);
 
@@ -98,7 +112,7 @@ class LimitRuleTest {
     @Test
     void predicate_aboveTopSource_shouldReturnFalse() {
         properties.put("lowerSourceId", "lower");
-        properties.put("topSourceId", "top");
+        properties.put("upperSourceId", "top");
         properties.set("sources", JsonNodeFactory.instance.arrayNode().add("s1"));
         LimitRule rule = new LimitRule("test", properties, mockResults);
 
