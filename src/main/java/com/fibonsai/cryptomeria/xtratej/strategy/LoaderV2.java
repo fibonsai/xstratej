7c3b8e7a0f1630571be0264697ba1e3202d4fe71
tuxmonteiro <me@tuxmonteiro.dev>
2026-03-02 15:08:36 +0100

    LoaderV2: Replace raw strings by constants

--- src/main/java/com/fibonsai/cryptomeria/xtratej/strategy/LoaderV2.java
+++ src/main/java/com/fibonsai/cryptomeria/xtratej/strategy/LoaderV2.java
@@ -26,50 +26,75 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import static com.fibonsai.cryptomeria.xtratej.strategy.LoaderV2.SchemaKey.*;
+
 /**
  * Strategy loader V2
  */
 public class LoaderV2 {
 
+    public enum SchemaKey {
+        STRATEGIES("strategies"),
+        SYMBOL("symbol"),
+        TYPE("type"),
+        SOURCES("sources"),
+        PUBLISHER("publisher"),
+        PARAMS("params"),
+        RULE("rule"),
+        INPUTS("inputs"),
+        ;
+
+        private final String keyName;
+
+        SchemaKey(String keyName) {
+            this.keyName = keyName;
+        }
+
+        public String key() {
+            return keyName;
+        }
+    }
+
+    private static final String UNDEF = "undef";
     private static final JsonNodeFactory NODE_FACTORY = JsonNodeFactory.instance;
 
     public static Map<String, IStrategy> fromJson(JsonNode json) {
 
         final Map<String, IStrategy> strategiesMap = new HashMap<>();
 
-        if (json.isObject() && json.hasNonNull("strategies")) {
-            Set<Map.Entry<String, JsonNode>> strategies = json.get("strategies").properties();
+        if (json.isObject() && json.hasNonNull(STRATEGIES.key())) {
+            Set<Map.Entry<String, JsonNode>> strategies = json.get(STRATEGIES.key()).properties();
             for (var strategyEntry: strategies) {
                 String strategyName = strategyEntry.getKey();
-                String strategySymbol = "undef";
+                String strategySymbol = UNDEF;
                 StrategyType strategyType = StrategyType.UNDEF;
                 JsonNode strategyJson = strategyEntry.getValue();
-                if (strategyJson.hasNonNull("symbol") && strategyJson.get("symbol").isString()) {
-                    strategySymbol = strategyJson.get("symbol").asString();
+                if (strategyJson.hasNonNull(SYMBOL.key()) && strategyJson.get(SYMBOL.key()).isString()) {
+                    strategySymbol = strategyJson.get(SYMBOL.key()).asString();
                 }
-                if (strategyJson.hasNonNull("type") && strategyJson.get("type").isString()) {
-                    String typeAsString = strategyJson.get("type").asString();
+                if (strategyJson.hasNonNull(TYPE.key()) && strategyJson.get(TYPE.key()).isString()) {
+                    String typeAsString = strategyJson.get(TYPE.key()).asString();
                     strategyType = StrategyType.fromName(typeAsString);
                 }
                 IStrategy strategy = new Strategy(strategyName, strategySymbol, strategyType);
 
                 // sources
-                if (strategyJson.hasNonNull("sources")) {
-                    Set<Map.Entry<String, JsonNode>> sources = strategyJson.get("sources").properties();
+                if (strategyJson.hasNonNull(SOURCES.key())) {
+                    Set<Map.Entry<String, JsonNode>> sources = strategyJson.get(SOURCES.key()).properties();
                     for (var sourceEntry: sources) {
                         String sourceName = sourceEntry.getKey();
                         JsonNode sourceJson = sourceEntry.getValue();
                         JsonNode sourceParams = NODE_FACTORY.nullNode();
                         SourceType sourceType = SourceType.UNDEF;
-                        String publisher = "undef";
-                        if (sourceJson.hasNonNull("type") && sourceJson.get("type").isString()) {
-                            sourceType = SourceType.fromName(sourceJson.get("type").asString());
+                        String publisher = UNDEF;
+                        if (sourceJson.hasNonNull(TYPE.key()) && sourceJson.get(TYPE.key()).isString()) {
+                            sourceType = SourceType.fromName(sourceJson.get(TYPE.key()).asString());
                         }
-                        if (sourceJson.hasNonNull("publisher") && sourceJson.get("publisher").isString()) {
-                            publisher = sourceJson.get("publisher").asString();
+                        if (sourceJson.hasNonNull(PUBLISHER.key()) && sourceJson.get(PUBLISHER.key()).isString()) {
+                            publisher = sourceJson.get(PUBLISHER.key()).asString();
                         }
-                        if (sourceJson.hasNonNull("params")) {
-                            sourceParams = sourceJson.get("params");
+                        if (sourceJson.hasNonNull(PARAMS.key())) {
+                            sourceParams = sourceJson.get(PARAMS.key());
                         }
                         Subscriber sourceInstance = sourceType.builder()
                                 .setName(sourceName)
@@ -80,8 +105,8 @@ public class LoaderV2 {
                 }
 
                 // rule (recursive structure)
-                if (strategyJson.hasNonNull("rule") && strategyJson.get("rule").isObject()) {
-                    Set<Map.Entry<String, JsonNode>> ruleEntries = strategyJson.get("rule").properties();
+                if (strategyJson.hasNonNull(RULE.key()) && strategyJson.get(RULE.key()).isObject()) {
+                    Set<Map.Entry<String, JsonNode>> ruleEntries = strategyJson.get(RULE.key()).properties();
                     for (var ruleEntry : ruleEntries) {
                         String rootRuleName = ruleEntry.getKey();
                         parseRule(rootRuleName, ruleEntry.getValue(), strategy);
@@ -98,16 +123,16 @@ public class LoaderV2 {
     private static void parseRule(String ruleName, JsonNode ruleJson, IStrategy strategy) {
         RuleType ruleType = RuleType.False;
         JsonNode ruleParams = NODE_FACTORY.nullNode();
-        if (ruleJson.hasNonNull("type") && ruleJson.get("type").isString()) {
-            ruleType = RuleType.fromName(ruleJson.get("type").asString());
+        if (ruleJson.hasNonNull(TYPE.key()) && ruleJson.get(TYPE.key()).isString()) {
+            ruleType = RuleType.fromName(ruleJson.get(TYPE.key()).asString());
         }
-        if (ruleJson.hasNonNull("params")) {
-            ruleParams = ruleJson.get("params");
+        if (ruleJson.hasNonNull(PARAMS.key())) {
+            ruleParams = ruleJson.get(PARAMS.key());
         }
         RuleStream ruleInstance = ruleType.builder().setId(ruleName).setProperties(ruleParams).build();
 
-        if (ruleJson.hasNonNull("inputs")) {
-            JsonNode inputs = ruleJson.get("inputs");
+        if (ruleJson.hasNonNull(INPUTS.key())) {
+            JsonNode inputs = ruleJson.get(INPUTS.key());
             if (inputs.isArray()) {
                 // Leaf rule taking sources as inputs
                 for (JsonNode input : inputs) {
