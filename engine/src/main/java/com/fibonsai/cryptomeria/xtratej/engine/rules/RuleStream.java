3950bb0160bf5a7fd28a663aa6a0e91d060752d9
tuxmonteiro <me@tuxmonteiro.dev>
2026-03-14 12:47:55 +0100

    RuleStream: add log support and prevent catch exceptions

--- engine/src/main/java/com/fibonsai/cryptomeria/xtratej/engine/rules/RuleStream.java
+++ engine/src/main/java/com/fibonsai/cryptomeria/xtratej/engine/rules/RuleStream.java
@@ -18,6 +18,8 @@ import com.fibonsai.cryptomeria.xtratej.engine.sources.Subscriber;
 import com.fibonsai.cryptomeria.xtratej.event.reactive.Fifo;
 import com.fibonsai.cryptomeria.xtratej.event.series.dao.*;
 import com.fibonsai.cryptomeria.xtratej.event.series.dao.builders.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import tools.jackson.databind.JsonNode;
 
 import java.util.LinkedHashSet;
@@ -26,6 +28,8 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 
 public abstract class RuleStream<T extends TimeSeries> {
+
+    private static final Logger log = LoggerFactory.getLogger(RuleStream.class);
 
     private final Fifo<TimeSeries> results = new Fifo<>();
     private final AtomicBoolean activated = new AtomicBoolean(false);
@@ -38,25 +42,30 @@ public abstract class RuleStream<T extends TimeSeries> {
 
     public void watch(Fifo<TimeSeries[]> inputs) {
         inputs.onSubscribe(() -> activated.set(true)).subscribe(inputTimeSeriesArray -> {
-            T[] resultTimeSeriesArray = predicate().apply(inputTimeSeriesArray);
-            if (resultTimeSeriesArray.length == 0) {
-                results.emitNext(EmptyTimeSeries.INSTANCE);
-            } else if (resultTimeSeriesArray.length == 1) {
-                results.emitNext(resultTimeSeriesArray[0]);
-            } else {
-                var ids = new LinkedHashSet<String>();
-                List.of(resultTimeSeriesArray).forEach(ts -> ids.add(ts.id()));
-                String newId = String.join("#", ids);
-                var builder = switch (resultTimeSeriesArray[0]) {
-                    case BooleanTimeSeries _ -> new BooleanTimeSeriesBuilder();
-                    case DoubleTimeSeries  _ -> new DoubleTimeSeriesBuilder();
-                    case Double2TimeSeries _ -> new Double2TimeSeriesBuilder();
-                    case BarTimeSeries _ -> new BarTimeSeriesBuilder();
-                    case BandTimeSeries _ -> new BandTimeSeriesBuilder();
-                    default -> throw new UnsupportedOperationException("not supported");
-                };
-                TimeSeries merged = builder.setId(newId).merge(resultTimeSeriesArray).build();
-                results.emitNext(merged);
+            try {
+                T[] resultTimeSeriesArray = predicate().apply(inputTimeSeriesArray);
+                if (resultTimeSeriesArray.length == 0) {
+                    results.emitNext(EmptyTimeSeries.INSTANCE);
+                } else if (resultTimeSeriesArray.length == 1) {
+                    results.emitNext(resultTimeSeriesArray[0]);
+                } else {
+                    var ids = new LinkedHashSet<String>();
+                    List.of(resultTimeSeriesArray).forEach(ts -> ids.add(ts.id()));
+                    String newId = String.join("#", ids);
+                    var builder = switch (resultTimeSeriesArray[0]) {
+                        case BooleanTimeSeries _ -> new BooleanTimeSeriesBuilder();
+                        case DoubleTimeSeries _ -> new DoubleTimeSeriesBuilder();
+                        case Double2TimeSeries _ -> new Double2TimeSeriesBuilder();
+                        case BarTimeSeries _ -> new BarTimeSeriesBuilder();
+                        case BandTimeSeries _ -> new BandTimeSeriesBuilder();
+                        default -> throw new UnsupportedOperationException("not supported");
+                    };
+                    TimeSeries merged = builder.setId(newId).merge(resultTimeSeriesArray).build();
+                    results.emitNext(merged);
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                results.emitError(e);
             }
         });
     }
