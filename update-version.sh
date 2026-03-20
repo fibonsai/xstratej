b2689dee84d9e95019c84e99e5fa57abf385e067
tuxmonteiro <me@tuxmonteiro.dev>
2026-03-20 16:58:24 +0100

    Improve update-version.sh

--- update-version.sh
+++ update-version.sh
@@ -4,4 +4,5 @@ mvn versions:commit
 git ch -b release-$(<VERSION)
 git commit -m "Release $(<VERSION)" VERSION `find . -name pom.xml | xargs echo -n`
 git tag $(<VERSION) -m "Release $(<VERSION)"
+git push origin release-$(<VERSION) 
 git push origin $(<VERSION)
